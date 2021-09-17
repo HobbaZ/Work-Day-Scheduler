d4c39607eee7b353618699d7ba54b677f798b96f
HobbZ <zachobba@gmail.com>
2021-09-17 23:27:38 +1000

    Moving along with the JS side of things

--- Develop/style.js
+++ Develop/style.js
@@ -4,12 +4,10 @@ $("#currentDay").text("Today is " + currentDay);
 
 //current events are red, future events are green, past events are grey
 
-
 currentHour = moment().format("H"); //in 24 hour format
 console.log("The current hour is " + currentHour);
 
 //set start and end times
-
 if (currentHour > 12) {
     $("#timeText").text((currentHour-12) + " pm");
     console.log(currentHour-12);
@@ -17,4 +15,25 @@ if (currentHour > 12) {
     $("#timeText").text(currentHour + " am");
 }
 
+if (currentHour) {
+
+}
+
 //get difference of start time and end time and create for loop that creates 3 sections for each hour
+var start = 9;
+var end = 17;
+var businessHours = end-start;
+
+for (let index = 0; index < businessHours; index++) {
+    //Create time section, input section and checkbox section
+    //need to assign a class name to the div so the bootstrap code will work hopefully
+    createDiv = document.createElement('div', {is: "form-check"});
+    createDiv.innerHTML = "<input class='form-check-input' type='checkbox' value='' id=flexCheckDefault'><label class='form-check-label' for='flexCheckDefault'>Completed</label>"
+    document.body.appendChild(createDiv);
+}
+    //generate sections to add bootstrap code
+
+
+
+    //var createCheckbox = document.querySelectorAll()
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Work-Day-Scheduler
+I've been tasked with completing the code for a work scheduler app, particularly the JS side of things. It will display the business hours with an textarea input and completed checkbox alongside each hour.
+
+### Screenshot
+
+### Technology Used
+Moment.JS, JS, HTML, CSS
+
+### Website link
